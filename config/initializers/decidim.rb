98c14fe4a2f09bec5509c7a398902e4b040231e7
Leonardo Diez <leiodd@gmail.com>
2018-11-12 17:49:50 +0100

    Locales configuration fixed

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -5,8 +5,8 @@ Decidim.configure do |config|
   config.mailer_sender = "change-me@domain.org"
 
   # Change these lines to set your preferred locales
-  config.default_locale = :en
-  config.available_locales = [:en, :ca, :es]
+  config.default_locale = :es
+  config.available_locales = [:es, :ca, :eu, :gl]
 
   # Geocoder configuration
   # config.geocoder = {
