113b05cfb7a480335597bf65548e7aacb74ec7ae
Leonardo Diez <leiodd@gmail.com>
2018-12-14 18:51:23 +0100

    Restrict Sidekiq Web UI access

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ require "sidekiq/web"
 
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   mount Decidim::Core::Engine => "/"
 
-  mount Sidekiq::Web => "/queues"
+  authenticate :user, ->(u) { u.admin? } do
+    mount Sidekiq::Web => "/queues"
+  end
 end
