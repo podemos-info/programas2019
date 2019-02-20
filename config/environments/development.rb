e74df2d787f7089c150d3c20ecab302ca97386a4
Leonardo Diez <leiodd@gmail.com>
2019-02-20 18:39:15 +0100

    Mailer development fixes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,13 +29,15 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.active_job.queue_adapter = :async
+
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :letter_opener_web
-  config.action_mailer.default_url_options = { port: 3000 }
+  config.action_mailer.default_url_options = {}
 
   config.action_mailer.perform_caching = false
 
